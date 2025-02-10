20da0ff4ebe5936fddd9e9ade421def504cf676f
JoshuaNwaizu <joshuachris000@gmail.com>
2025-02-10 18:02:06 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { BiPhoneCall } from 'react-icons/bi';
+
+const App = () => {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center text-6xl font-bold italic text-black">
+      HANGMAN <BiPhoneCall />
+    </div>
+  );
+};
+
+export default App;
